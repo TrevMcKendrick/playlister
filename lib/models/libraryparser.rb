d75637e1ebcd74f6d999d6def3493bde0f631af7
Kyle Shike <kyleshike@gmail.com>
2013-10-16 18:22:47 -0400

    objects w duplicates

--- lib/models/libraryparser.rb
+++ lib/models/libraryparser.rb
@@ -12,47 +12,42 @@ class LibraryParser
       song_name = item.split(" - ")[1].split(" [")[0]
       genre_name = item.split(" - ")[1].split(" [")[1].gsub("].mp3","")
 
-      Song.new.tap do |s| 
-        s.name = song_name
-        set_genre 
-        set_artist
+
+      temp = Song.new.tap{ |s| s.name = song_name }
+      Artist.new.tap do |a| 
+        a.name = artist_name
+        a.add_song(temp)
       end
+      Genre.new.tap{ |g| g.name = genre_name }
+
+      # temp_song = Song.new.tap{ |s| s.name = song_name }
+
+      # artist_names = Artist.all.collect do |artist|
+      #   artist.name if artist
+      # end
+
+      # if artist_names.include?(artist_name)
+      #   Artist.all.each do |artist|
+      #     artist.add_song(temp_song) if artist.name == artist_name
+      #   end
+      # else 
+      #   Artist.new.tap do |artist|
+      #     artist.name = artist_name
+      #     artist.add_song(temp_song)
+      #   end
+      # end
     end
   end
-
-  def set_genre
-    if genre
-  end
-
-  def set_artist
-  end
-
 end
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-# def exists?
-#   #check if it exists
-# end
-
-# def assign(object) 
-#   if !exists? 
-#     object.new.tap{ |o| o.name = genre_name}
-#   elsif condition
-    
-#   end
-# end 
-
-
-
+      # Artist.new.tap do |a| 
+      #   a.name = artist_name 
+      #   a.add_song(Song.new.tap do |s| 
+      #     s.name = song_name 
+      #     s.genre = (Genre.new.tap{ |g| g.name = genre_name })
+      #   end)
+      # end
