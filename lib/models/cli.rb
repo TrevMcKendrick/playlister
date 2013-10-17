3f00c6c1ea0b64e532bc7e399db8d81195f4a82a
Kyle Shike <kyleshike@gmail.com>
2013-10-17 14:50:09 -0400

    add play function to cli browser

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -28,6 +28,7 @@ class Cli
     case type
       when "songs"
         songs.each{|song| puts song.name }
+        play_option
       when "artists"
         artists.each{|artist| puts artist.name}
         puts "Select an artist to see all the songs"
@@ -47,11 +48,16 @@ class Cli
     end
   end
 
-  # def play_option
-  #   puts "select a song to play it, or type back to return to the main menu"
-  #   # input = gets.strip.downcase
-    
-  # end
+  def play_option
+    puts "select a song by its name to play it, type back to return to the main menu"
+    # input = gets.strip.downcase
+    str = gets.strip.downcase
+    exit if str == "exit"
+    return if str == "back" || str == "cancel"
+    songs.each do |song|
+      puts "Now Playing: #{song.name} by #{song.artist.name}" if str.downcase == song.name.downcase
+    end
+  end
 
   def browse_by_genre(str)
     exit if str == "exit"
@@ -59,6 +65,7 @@ class Cli
     songs.each do |song|
       puts "#{song.artist.name} - #{song.name}" if str.downcase == song.genre.name.downcase
     end
+    play_option
   end
 
   def browse_by_artist(str)
@@ -67,6 +74,7 @@ class Cli
     songs.each do |song|
       puts "#{song.name} - #{song.genre.name}" if str.downcase == song.artist.name.downcase
     end
+    play_option
   end 
 
   def all(object)
